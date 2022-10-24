a806eae4fd4fae0a2054bbe60481268fd708ffae
SiansandaTamara <tamarasiansanda18@gmail.com>
2022-10-24 10:40:52 +0200

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Greeting from './Greating';
+import Greeting from './Greetings';
 
 const App = () => (
   <div className="App">
